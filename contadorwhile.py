141a6c81943c512f60803c397ea4203b4c106006
amandafig <111015122+amandafig@users.noreply.github.com>
2023-01-20 17:02:50 -0300

    VINTE DE JANEIRO

---
+++ contadorwhile.py
@@ -0,0 +1,12 @@
+# contadores com while
+x = 1
+print ("Contador progressivo: ")
+while x <= 10:
+    print(x)
+    x += 1
+
+print ("Contador regressivo: ")
+y=10
+while y>=1:
+    print (y)
+    y-=1
